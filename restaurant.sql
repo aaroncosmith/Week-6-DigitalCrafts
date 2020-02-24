05a436eefe5a216e7da69c93d893e1eae7f4eae6
Aaron Smith <aaroncosmith@gmail.com>
2020-02-24 15:01:18 -0500

    started node practice

--- restaurant.sql
+++ restaurant.sql
@@ -1,12 +1,36 @@
+-- CREATE TABLE restaurant(
+--     id SERIAL PRIMARY KEY,
+--     name text,
+--     distance int,
+--     stars int,
+--     category text,
+--     favorite_dish text,
+--     takeout text,
+--     last_visit date);
+
+-- INSERT INTO restaurant VALUES (1, 'Farm Burger', 0.3, 5, 'American', 'Farm-burger','Yes','02-21-2020');
+-- INSERT INTO restaurant VALUES (2, 'Chick-flia', 0.5, 4, 'Fast-Food', 'Chicken-sandwhich', 'Yes', '02-07-2020');
+
 CREATE TABLE restaurant(
     id SERIAL PRIMARY KEY,
     name text,
-    distance int,
-    stars int,
-    category text,
-    favorite_dish text,
-    takeout text,
-    last_visit date);
+    address text,
+    category text
+);
 
-INSERT INTO restaurant VALUES (1, 'Farm Burger', 0.3, 5, 'American', 'Farm-burger','Yes','02-21-2020');
-INSERT INTO restaurant VALUES (2, 'Chick-flia', 0.5, 4, 'Fast-Food', 'Chicken-sandwhich', 'Yes', '02-07-2020');
+CREATE TABLE reviewer(
+    id SERIAL PRIMARY KEY,
+    name text,
+    email text,
+    karma numeric CHECK (karma <= 7 and karma >= 0)
+);
+
+CREATE TABLE review(
+    id SERIAL PRIMARY KEY,
+    reviewerid INT REFERENCES review(id)
+    -- REVIEWER ID USING A FOREIGN KEY CONSTRAINT
+    stars numeric CHECK (stars >= 1 and stars <= 5),
+    title text,
+    review text,
+    restaurantid INT REFERENCES restaurant(id)
+);
